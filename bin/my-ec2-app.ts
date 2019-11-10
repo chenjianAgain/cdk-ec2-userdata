073e79bc7124defe241a2fa917d0a591b5dd3ce1
joseph <joseph.siyi@gmail.com>
2019-11-10 08:44:47 +0800

    Added userdata for ec2

--- bin/my-ec2-app.ts
+++ bin/my-ec2-app.ts
@@ -1,7 +1,14 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import cdk = require('@aws-cdk/core');
-import { MyEc2AppStack } from '../lib/my-ec2-app-stack';
+import { CdkPostStack } from '../lib/my-ec2-app-stack';
+import { MyEc2AppStack } from '../lib/MyEc2AppStack';
 
+
+const env = {
+    region: 'us-west-2',
+    account: '374801192098'
+};
 const app = new cdk.App();
-new MyEc2AppStack(app, 'MyEc2AppStack');
+new CdkPostStack(app, 'CdkPostStack', { env });
+new MyEc2AppStack(app, 'MyEc2AppStack', { env });
--- lib/my-ec2-app-stack.ts
+++ lib/my-ec2-app-stack.ts
@@ -1,9 +1,18 @@
+import sns = require('@aws-cdk/aws-sns');
+import subs = require('@aws-cdk/aws-sns-subscriptions');
+import sqs = require('@aws-cdk/aws-sqs');
 import cdk = require('@aws-cdk/core');
 
-export class MyEc2AppStack extends cdk.Stack {
-  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+export class CdkPostStack extends cdk.Stack {
+  constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const queue = new sqs.Queue(this, 'CdkPostQueue', {
+      visibilityTimeout: cdk.Duration.seconds(300)
+    });
+
+    const topic = new sns.Topic(this, 'CdkPostTopic');
+
+    topic.addSubscription(new subs.SqsSubscription(queue));
   }
 }
